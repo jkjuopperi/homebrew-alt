d1d4e2648daa168df97922cf5f82090b894c89cd
Juho Juopperi <jkj@remod.fi>
2012-10-26 20:11:28 +0300

    engine_pkcs11 0.1.8

---
+++ engine_pkcs11.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class EnginePkcs11 < Formula
+  url 'http://www.opensc-project.org/files/engine_pkcs11/engine_pkcs11-0.1.8.tar.gz'
+  homepage 'http://www.opensc-project.org/engine_pkcs11'
+  md5 '693b402eafba152666e66431ed4c1b9e'
+
+  depends_on 'jkjuopperi/alt/libp11'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
