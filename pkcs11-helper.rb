0c09b75e511a897ec87a4e1d83adb9e4fbc60d2c
Juho Juopperi <jkj@remod.fi>
2012-10-26 20:11:46 +0300

    pkcs11-helper 1.09

---
+++ pkcs11-helper.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Pkcs11Helper < Formula
+  url 'http://www.opensc-project.org/files/pkcs11-helper/pkcs11-helper-1.09.tar.bz2'
+  homepage 'http://www.opensc-project.org/opensc/wiki/pkcs11-helper'
+  md5 '88ca59143f1b1d36283cab406f33a3fa'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
